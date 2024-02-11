bc76cbb2ed17ae625d83bbc52b2faaf5d216a41d
Farhan933 <farhanfarabiemon@gmail.com>
2024-02-11 20:47:27 +0600

    adding user api and crud operation

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
 app.use(cors())
 app.use(express.json());
 
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zdzdyrx.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,7 +45,154 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const clothesCollection = client.db("luminaStore").collection("clothes");
+        const orderCollection = client.db("luminaStore").collection("order");
+        const userCollection = client.db("luminaStore").collection("user");
 
+
+
+        // {----------jwt api----------}
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        })
+
+        // It's a middleware checking a user is Admin or not in the database. That's why it's writting in between mongodb.
+        //Warning: use verifyJWT before using verifyAdmin.
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access.' })
+            }
+            next();
+        }
+
+        // {----------user api----------}
+        app.get('/user', verifyJWT, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+        // {----------admin api----------}
+        app.get('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email != email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
+        app.patch('/user/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/user/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // {----------clothes api----------}
+        app.get('/clothes', async (req, res) => {
+            const result = await clothesCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/clothes/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await clothesCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.patch('/clothes/category/:id', async (req, res) => {
+            const product = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    short_description: product.short_description,
+                    new_price: product.new_price,
+                    old_price: product.old_price,
+                    category: product.category,
+                    image: product.image,
+                }
+            }
+
+            const result = await clothesCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.post('/clothes', verifyJWT, verifyAdmin, async (req, res) => {
+            const product = req.body;
+            const result = await clothesCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.delete('/clothes/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await clothesCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // {----------order api----------}
+        app.get('/order', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email != decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/order', async (req, res) => {
+            const item = req.body;
+            const result = await orderCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
